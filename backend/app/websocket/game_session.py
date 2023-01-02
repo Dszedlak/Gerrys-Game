8fe6e7fb138ba8b2ebcd57baeed3c233e8e8a768
David Szedlak <Dszedlak@gmail.com>
2023-01-02 23:58:17 +0000

    Investigate

--- backend/app/websocket/game_session.py
+++ backend/app/websocket/game_session.py
@@ -1,19 +1,21 @@
 from app.models import Room, User, db, RoomParticipants
 import json
 from threading import Lock
+import threading
+import time
 from flask_socketio import emit, join_room, leave_room, send
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import socketio
 from flask import current_app as app
 
-thread = None
-thread_lock = Lock()
-
 class GameSession():
+
     def __init__(self, roomname):
         self._room = db.session.query(Room.id).filter_by(name=roomname).first()[0]
         self._socketio = socketio
         self.isRunning = False
+        self.thread = None
+        self.thread_lock = Lock()
 
     def getRoom(self) -> int:
         return self._room
@@ -33,20 +35,20 @@ class GameSession():
             #tmp = {"User": db.session.query(User.username).filter_by(id=participant.userId).first()[0], "Job": participant.job}
         jsonString = json.dumps(players)
             
-        print(jsonString)    
+        print(jsonString)
 
         return jsonString
         #return json.dumps(participant, default=obj_dict)
 
     def run(self):
         self.isRunning = True
-        global thread
-        with thread_lock:
-            if thread is None:
-                    thread = self._socketio.start_background_task(self.background_thread())
+        with self.thread_lock:
+            if self.thread is None:
+                self.thread =  self._socketio.start_background_task(self.background_thread())
 
     def background_thread(self):
         while True:
             participants = self.load_ready_partipants()
-            emit('UpdateUserStatus', {'data':participants}, to=self._room)
+            print(participants)
+            self._socketio.emit('UpdateUserStatus', {'data':participants}, to=self._room)
             self._socketio.sleep(1)
--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import createApp, socketio
 from os import getenv
+from app.websocket.game_session import GameSession
 
 configName = getenv("FLASK_ENV")
 app = createApp(configName)
@@ -7,18 +8,9 @@ app = createApp(configName)
 @app.before_first_request
 def before_first_request_func():
     with app.app_context():
-        from app.websocket.game_session import GameSession
-
         game = GameSession("test")
         game.run()
-
-        gametwo = GameSession("roogy")
-        gametwo.run()
-
-        print("YourMum")
-        print("YourMum")    
-        print("YourMum")
-
+        print("cock")
+        
 if __name__ == "__main__":
-    print(app.before_first_request_funcs)
     socketio.run(app)
